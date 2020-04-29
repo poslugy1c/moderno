a4a317c1068d9d8564b5095441e1d47756a94d3b
poslugy1c <poslugy1c@gmail.com>
2020-04-29 20:52:27 +0300

    added upload  page

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,8 @@ gulp.task('style', function(){
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
     'node_modules/ion-rangeslider/css/ion.rangeSlider.css', 
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.css', 
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css', 
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
   ])
   .pipe(concat('libs.min.css'))
@@ -40,6 +42,7 @@ gulp.task('script', function(){
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js', 
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
   ])
   .pipe(concat('libs.min.js'))
